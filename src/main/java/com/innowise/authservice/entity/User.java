5ee241389f4dcbd076ac31cb9fba905712bf2b82
lexavodoprovod <nikitoshololeenko18@gmail.com>
2026-04-28 13:35:59 +0300

    fix: change @Data to @Getter@Setter

--- src/main/java/com/innowise/authservice/entity/User.java
+++ src/main/java/com/innowise/authservice/entity/User.java
@@ -1,18 +1,14 @@
 package com.innowise.authservice.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
-
-@Data
+@Getter@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
